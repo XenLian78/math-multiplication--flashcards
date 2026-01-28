63c56ad4714b162a3dacca8894b92d5aaa77ddc7
XenLian78 <xenlian@yahoo.gr>
2026-01-28 10:14:28 +0200

    Update streamlit_math_app.py

--- streamlit_math_app.py
+++ streamlit_math_app.py
@@ -26,7 +26,7 @@ css_code = """
 
 .flip-card-inner { 
     position: relative; width: 100%; height: 100%; text-align: center; 
-    transition: transform 0.1s; transform-style: preserve-3d; 
+    transition: transform 0.0s; transform-style: preserve-3d; 
 }
 
 .do-flip { transform: rotateY(180deg); }
